111af4e5b4c08032bd456e052c8af152d1cc2688
beatrice2000 <126506911+beatrice2000@users.noreply.github.com>
2025-06-05 02:06:48 +0300

    Update BookSearchRepository.java

--- book-network/src/main/java/com/beatrice/book/search/BookSearchRepository.java
+++ book-network/src/main/java/com/beatrice/book/search/BookSearchRepository.java
@@ -2,7 +2,6 @@ package com.beatrice.book.search;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-@Autowired(required = false)
 public interface BookSearchRepository extends ElasticsearchRepository<BookDocument, String> {
 }
 
