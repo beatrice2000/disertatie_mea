b71c15858802633f7282fb75c0d76125cafee478
beatrice2000 <blidaru.beatrice21@gmail.com>
2025-06-11 10:13:03 +0300

    test8

--- book-network/src/test/java/com/beatrice/book/BookNetworkApiApplicationTests.java
+++ book-network/src/test/java/com/beatrice/book/BookNetworkApiApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootTest
-@EnableElasticsearchRepositories
+@SpringBootTest(classes = BookNetworkApiApplication.class)
 class BookNetworkApiApplicationTests {
 
 	@Test
